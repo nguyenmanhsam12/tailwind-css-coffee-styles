c1d575b4c2ab2233dfb64d628246c8b3f0c9af6b
nguyenmanhsam12 <150453510+nguyenmanhsam12@users.noreply.github.com>
2025-04-06 16:02:05 +0700

    responsive_header (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+4/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        Karla: ['Karla','sans-serif']
+      },
+      colors : {
+        'light-coffee' : '#C89F94'
+      },
+      keyframes: {
+        slideDown : {
+          '0%' : { transform : 'translateY(-100%)' },
+          '100%' : { transform : 'translateY(0)' },
+        },
+        
+      },
+      animation : {
+        slideDown : 'slideDown .4s ease-in-out',
+      }
+    },
   },
   plugins: [],
 }
